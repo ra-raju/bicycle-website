2ac6a1c10c8d6f430264412dc21271dfa8dc032f
robeyoul-awal-raju <robeyoulawal@gmail.com>
2021-11-12 11:34:29 +0600

    hero section complete

---
+++ src/Pages/Homepage/Hero/Hero.jsx
@@ -0,0 +1,39 @@
+import { Button, Grid } from '@mui/material';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import hero from '../../../images/hero.jpg';
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <div className="hero-section">
+      <Grid container spacing={2}>
+        <Grid item sm={12} md={6}>
+          <div className="hero-content">
+            <h1>
+              Ride Your Bike <br /> to Havean with Us
+            </h1>
+            <p className="hero-para">
+              Our shop provides varities of bicycle with best design and cheap
+              price. Adjust your funds and needs, to realize healthy and fit
+              body.
+            </p>
+            <Button variant="contained">
+              <Link
+                to="/products"
+                style={{ textDecoration: 'none', color: 'white' }}
+              >
+                All Products
+              </Link>
+            </Button>
+          </div>
+        </Grid>
+        <Grid item sm={12} md={6}>
+          <img src={hero} alt="" className="hero-img" />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Hero;
--- src/Pages/Homepage/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage/Homepage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navigation from '../../Sharedpage/Navigation/Navigation';
+import Hero from '../Hero/Hero';
 import Services from '../Services/Services/Services';
 
 const Homepage = () => {
   return (
     <div>
       <Navigation />
+      <Hero />
       <Services />
     </div>
   );
